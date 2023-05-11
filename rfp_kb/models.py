d5c59ac006295e397e82849921f31fa0b1733463
mimmolamparelli <mimmo.lamparelli@gmail.com>
2023-05-11 20:42:56 +0200

    Add,Update,Delete Buttons

--- rfp_kb/models.py
+++ rfp_kb/models.py
@@ -8,6 +8,8 @@ class rfp_bk(models.Model):
     answer = models.CharField(max_length=2048)
     winLoss = models.BooleanField()
     comply = models.BooleanField()
+    product = models.CharField(max_length=200, null=True ) #this will be a lookup field
+    product_variant = models.CharField(max_length=200, null = True)
 
     def __str__(self):
         return self.question
--- rfp_kb/views.py
+++ rfp_kb/views.py
@@ -8,19 +8,25 @@ from .models import rfp_bk
 # github #2
 def rfp_kb(request, obj_id=0):
     # print(request.POST.get('id'))
-    name = ""
-    q = ""
-    a = ""
+    name = "" #rfp_name
+    q = "" #question
+    a = "" # answer
+    p = ""  # product 
+    v="" #product_variant
     if obj_id > 0:
         selected_item = rfp_bk.objects.filter(id=obj_id).values()
         name = selected_item[0]["rfp_name"]
         q = selected_item[0]["question"]
         a = selected_item[0]["answer"]
+        p = selected_item[0]["product"]
+        v = selected_item[0]["product_variant"]
     template = loader.get_template('main.html')
     context = {
         "rfp_name": name,
         "question": q,
         "answer": a,
+        "product":p,
+        "product_variant":v,
     }
     return HttpResponse(template.render(context, request))
 
@@ -39,7 +45,7 @@ def rfp_qa_list(request):
     a = request.POST.get('txtAnswer')
     print(f"Question:{q} / Answer:{a}")
     # mydata = Member.objects.all()
-    elems = rfp_bk.objects.all().values()
+    elems = rfp_bk.objects.filter(question__icontains=q, answer__icontains=a).values()
     template = loader.get_template('rfp_qa_list.html')
     context = {
         "elems": elems,
